b76277ad4ef5c096e682e07ecfe6488353144020
parksiwoong <tldnd5656@naver.com>
2023-02-21 23:19:32 +0900

    아코디언 만들기

--- accordion_1.js
+++ accordion_1.js
@@ -1,6 +1,13 @@
 window.onload =()=>{
+    //panel-faq-container
     const panelFaqContainer = document.querySelectorAll('.panel-faq-container');
     console.log(panelFaqContainer); //NodeList 객체
+
+    //btn-all-close
+    const btnAllClose = document.getElementById('btn-all-close');
+    console.log(btnAllClose);
+
+    let panelFaqAnswer= document.querySelectorAll('.panel-faq-answer');
 
     //반복문 순회하면서 해당 FAQ제목 클릭시 콜백 처리
     for(let i=0;i< panelFaqContainer.length; i++){
@@ -8,11 +15,16 @@ window.onload =()=>{
             //클릭시 처리 할 일
             console.log('나클릭..');
             //FAQ 제목 클릭시 --> 본문이 보이게끔 --> active 클래스 추가 .
-
             //본문선택
-            let panelFaqAnswer= document.querySelectorAll('.panel-faq-answer');
+
             // this.classList.add('active'); // 이렇게하면  디스는 panelFaqContainer 를 가르키기 때문에 안됨
             panelFaqAnswer[i].classList.add('active');
         })
     }
+
+    btnAllClose.addEventListener('click',function(){
+        for(let i=0;i<panelFaqAnswer.length; i++){
+            panelFaqAnswer[i].classList.remove('active');
+        }
+    })
 }
